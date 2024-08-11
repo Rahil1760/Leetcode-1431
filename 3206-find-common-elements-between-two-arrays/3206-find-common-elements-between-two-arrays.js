78fef0b585b8d6d4e34b008c5f5bb5fd590a1f64
Rahil1760 <100726685+Rahil1760@users.noreply.github.com>
2024-08-11 20:28:37 +0530

    Time: 96 ms (24.72%), Space: 53.7 MB (69.13%) - LeetHub

---
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var findIntersectionValues = function(nums1, nums2) {
+    let result = [0,0]
+const linearSearch = (nums,k)=>{
+    for(let i = 0;i<nums.length;i++){
+        if(nums[i] == k){
+            return true;
+        }
+        
+        
+    }
+    return false;
+}
+    for(let i = 0;i <nums1.length; i++){
+        if(linearSearch(nums2,nums1[i])){
+            result[0] += 1;
+        }
+    }
+
+    
+    for(let i =0; i< nums2.length; i++){
+        if(linearSearch(nums1,nums2[i])){
+            result[1] += 1;
+        }
+    }
+    return(result)
+};
