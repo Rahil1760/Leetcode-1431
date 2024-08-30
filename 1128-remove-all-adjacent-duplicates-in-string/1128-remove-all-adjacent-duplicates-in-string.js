171813a795ae7d387f5a081dcd7a79b5f49312b2
Rahil1760 <100726685+Rahil1760@users.noreply.github.com>
2024-08-30 15:57:34 +0530

    Time: 88 ms (8.69%), Space: 57.2 MB (79.75%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+       const stack = [];
+    for (let char of s) {
+        if (char === stack[stack.length-1]) stack.pop();
+        else {
+          stack.push(char);
+        }
+    }
+    return stack.join("");
+};
